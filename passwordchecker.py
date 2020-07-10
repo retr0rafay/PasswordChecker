83e73c47fe0a976a985273bce48e2bbc56b8b56c
rafay.syed <rafay.syed@salesforce.com>
2020-07-10 11:39:35 -0400

    Finished creating the password checker

--- passwordchecker.py
+++ passwordchecker.py
@@ -1,5 +1,6 @@
 import requests
 import hashlib
+import sys
 
 
 def request_api_data(query_char):
@@ -10,7 +11,31 @@ def request_api_data(query_char):
     return password_api_response
 
 
+def get_password_leaks_count(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        if hash_to_check == h:
+            return int(count)
+    return 0
+
+
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    return sha1password
+    first_five_chars, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first_five_chars)
+    return get_password_leaks_count(response, tail)
 
+
+def main(args):
+    passwords = input('Enter some passwords to check if they have been leaked. Make sure to separate each password '
+                      'with a space.\n').split()
+    for pw in passwords:
+        number_of_leaks = pwned_api_check(pw)
+        if number_of_leaks == 0:
+            print(f'Password {pw} is good!')
+        else:
+            print(f'Oh no! Your password {pw} has been leaked {number_of_leaks} times! Please change it ASAP!')
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
